09a45a9d168577150480e57c28465838e9d4c223
ksi2564 <kimsungin5@gmail.com>
2023-03-10 09:31:09 +0900

    김성인 : 백준 1520 내리막길

--- src/seongin/week01/P1520_내리막길.java
+++ src/seongin/week01/P1520_내리막길.java
@@ -3,13 +3,12 @@ package seongin.week01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class P1520_내리막길 {
-    static int M, N, result;
+    static int M, N;
     static int[][] map;
-    static boolean[][] visit;
+    static int[][] dp;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
@@ -17,41 +16,38 @@ public class P1520_내리막길 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        result = 0;
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
         map = new int[M][N];
-        visit = new boolean[M][N];
+        dp = new int[M][N];
 
-        visit[0][0] = true;
-        dfs(0, 0);
-        visit[0][0] = false;
-//        for (int i = 0; i < M; i++) {
-//            System.out.println(Arrays.toString(visit[i]));
-//        }
-        System.out.println(result);
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1; // 0이면 주변 인접 지역 못가는 애랑 한번도 체크 안해본 애랑 구분 안되니까
+            }
+        }
+
+        System.out.println(dfs(0, 0));
     }
 
-    static void dfs(int x, int y) {
+    static int dfs(int x, int y) {
         if (x == M - 1 && y == N - 1) {
-            result++;
-            return;
-        }
-        for (int i = 0; i < M; i++) {
-            System.out.println(Arrays.toString(visit[i]));
+            return 1;
         }
 
-        for (int d = 0; d < 4; d++) {
-            int nx = x + dx[d];
-            int ny = y + dy[d];
+        if (dp[x][y] == -1) {
+            dp[x][y] = 0;
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
 
 
-
-            if (nx < 0 || ny < 0 || nx >= M || ny >= N || visit[nx][ny] || map[x][y] <= map[nx][ny]) continue;
-            visit[nx][ny] = true;
-            dfs(nx, ny);
-            visit[nx][ny] = false;
+                if (nx < 0 || ny < 0 || nx >= M || ny >= N || map[x][y] <= map[nx][ny]) continue;
+                dp[x][y] += dfs(nx, ny);
+            }
         }
-
+        return dp[x][y];
     }
 }
